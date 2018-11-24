bfb236963e6796e384628da33c409c74c4207d52
nttlong <zugeliang2000@gamil.com>
2018-11-24 18:29:26 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import pyparams_validator
+setup(
+    name='pyparams_validators',
+    version=pyparams_validator.get_version(),
+    packages=['pyparams_validator'],
+    url='',
+    license='',
+    author='nttlong',
+    author_email='zugeliang2000@gmail.com',
+    description='method validate param decorator'
+)
